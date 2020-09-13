0407f44e1b293c3202ed52cf109bc60ce0e05b49
Cesario <cipoca21@gmail.com>
2020-09-13 03:30:14 -0300

    Primer commit, subiendo servidor REST

---
+++ server/config/config.js
@@ -0,0 +1,9 @@
+//process objeto global que corre en node.js
+
+
+//====================================
+//  Puerto
+//====================================
+
+//Si el puerto no existe nosotros damos el valor
+process.env.PORT = process.env.PORT || 3000;
---
+++ server/server.js
@@ -0,0 +1,48 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+require('./config/config')
+
+
+//MIDDLEWARE
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+
+app.get('/usuario', (req, res) => {
+    res.json('get Usuario')
+})
+
+app.post('/usuario', (req, res) => {
+    let body = req.body;
+
+    if (body.nombre === undefined) {
+        res.status(400).json({
+            ok: false,
+            mensaje: 'El nombre es necesario'
+        });
+    }
+    res.json({
+        persona: body
+    })
+})
+
+app.put('/usuario/:id', (req, res) => {
+
+    let id = req.params.id;
+
+    res.json({
+        id
+    })
+})
+
+app.delete('/usuario', (req, res) => {
+    res.json('delete Usuario')
+})
+
+app.listen(process.env.PORT, () => {
+    console.log('ejecutando desde el puerto 3000')
+})
