9b6651e17d10bd8b363d129af926c631753bc3e4
Cesario <cipoca21@gmail.com>
2020-09-23 04:15:11 -0300

    env variable

--- server/config/config.js
+++ server/config/config.js
@@ -26,7 +26,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://user_admin:R7fkGFtf89jWj4zW@cluster0.aq8az.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
