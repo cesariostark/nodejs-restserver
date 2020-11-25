b6c1d444a1812558082c9f00f4d257a9efe8f5b8
Cristian Reyes <cristian.alonso.reyes@gmail.com>
2020-11-25 18:33:02 -0300

    db local y servidor

--- server/config/db.js
+++ server/config/db.js
@@ -1,13 +1,20 @@
 const mysql = require('mysql');
 
-//Conexion a BD
+//Conexion a BD local
+// const pool = mysql.createPool({
+//   host: 'localhost',
+//   user: 'root',
+//   password: 'password',
+//   database: 'transapp',
+//   insecureAuth: true
+// });
+//conexion heroku
 const pool = mysql.createPool({
-    host     : 'us-cdbr-east-02.cleardb.com',
-    user     : 'bf22a30332897b',
-    password : 'b16e137c',
-    database : 'heroku_fd294c00043b63f',
-    insecureAuth: true
-  });
+  host: 'us-cdbr-east-02.cleardb.com',
+  user: 'bf22a30332897b',
+  password: 'b16e137c',
+  database: 'heroku_fd294c00043b63f',
+  insecureAuth: true
+});
 
 module.exports = pool;
-
