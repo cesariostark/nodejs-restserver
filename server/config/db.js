cb3b992a721ee6914351fb4b2769356a1a02d54e
Cesario <cipoca21@gmail.com>
2020-12-02 21:42:02 -0300

    Cambio de variables de entorno

--- server/config/db.js
+++ server/config/db.js
@@ -2,10 +2,10 @@ const mysql = require('mysql');
 
 //Conexion a BD local
 const pool = mysql.createPool({
-  host: process.env.HOST,
-  user: process.env.USER,
-  password: process.env.PASSWORD,
-  database: process.env.DB,
+  host: 'de1tmi3t63foh7fa.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+  user: 'h1dnajsq6r2gyute',
+  password: 'mudiro4d6q3a1y0v',
+  database: 'mzf63cxl54rnezbh',
   insecureAuth: true
 });
 
